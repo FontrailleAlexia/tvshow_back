61fa96a8f6e41fa4b2983338fe2f5b1f1eabc154
yaelx <yaelx@localhost>
2022-02-25 19:05:45 +0100

    install symfony + create user

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/api/register", name="api_register", methods="POST")
+     */
+    public function register(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordEncoder, Request $request, ValidatorInterface $validator, SerializerInterface $serializer): Response
+    {
+        $jsonContent = $request->getContent();
+        
+        $user = $serializer->deserialize($jsonContent, User::class, 'json');
+
+        $errors = $validator->validate($user);
+        
+        // If there is at least one error, we return a 400
+        if (count($errors) > 0) {
+            $errorsList = [];
+            foreach ($errors as $erreur) {
+                $input = $erreur->getPropertyPath();
+                $errorsList[$input] = $erreur->getMessage();
+            }
+
+            return $this->json(
+                [
+                    'error' => $errorsList
+                ],
+                400
+            );
+        }
+       
+        $password = $user->getPassword();
+        // This is where we encode the User password (found in $ user)
+        $encodedPassword = $passwordEncoder->hashPassword($user, $password);
+        // We reassign the password encoded in the User
+        $user->setPassword($encodedPassword);
+        $user->setRoles(['ROLE_USER']);
+        $user->setCreatedAt(new \DateTimeImmutable());
+
+        // We save the user
+        $entityManager->persist($user);
+        $entityManager->flush();
+          
+        return $this->json([
+                'user' => $user
+            ], Response::HTTP_CREATED);
+    }
+    
+    /**
+     * @Route("/api/users", name="api_users", methods="GET")
+     */
+    /*
+    public function user(UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
+        $users = $userRepository->findByUserField($user);
+       
+
+        return $this->json($users, 200, [], ['groups' => 'user_read']);
+    }
+    */
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $created_at;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
